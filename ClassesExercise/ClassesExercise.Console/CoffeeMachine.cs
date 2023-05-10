1d38e0f4a7a4727c4a50e5f21447df417dcd66bb
Masooglu Simon <simon.masooglu@liebherr.com>
2023-05-10 20:26:48 +0200

    Added new class example exercise

---
+++ ClassesExercise/ClassesExercise.Console/CoffeeMachine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExercise.Console
+{
+    public class CoffeeMachine
+    {
+        public string color;
+        public int power;
+
+        public void SetGrindingDegree(int degree)
+        {
+            System.Console.WriteLine($"Set grinding degree to: {degree}");
+        }
+
+        public void SetGrindingPower(int power)
+        {
+            this.power = power;
+            System.Console.WriteLine($"Set grinding power to: {power}");
+        }
+
+        public void CreateBigCoffee()
+        {
+            StartHeater();
+            System.Console.WriteLine($"{color} maschine creating a coffee with a big amount of water.");
+        }
+
+        public void CreateSmallCoffee()
+        {
+            StartHeater();
+            System.Console.WriteLine($"{color} maschine creating a coffee with a small amount of water.");
+        }
+
+        private void StartHeater()
+        {
+            System.Console.WriteLine($"{color} maschine starting the heater.");
+        }
+    }
+}
---
+++ ClassesExercise/ClassesExercise.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using ClassesExercise.Console;
+using System.Drawing;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CoffeeMachine redCoffeeMachine = new CoffeeMachine();
+        redCoffeeMachine.color = "red";
+
+        CoffeeMachine greenCoffeeMachine = new CoffeeMachine();
+        greenCoffeeMachine.color = "green";
+
+        redCoffeeMachine.CreateSmallCoffee();
+        greenCoffeeMachine.CreateSmallCoffee();
+    }
+}
